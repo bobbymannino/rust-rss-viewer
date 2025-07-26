44b50d89743c06a0cd662f97f405c2f9945c3ed2
Bob Mannino <manninobobby@icloud.com>
2025-07-26 21:26:06 +0100

    get hello world frame working

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,37 @@
-fn main() {}
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([500.0, 500.0])
+            .with_min_inner_size([500.0, 500.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "RSS Viewier",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("RSS Viewer");
+            ui.label("Hi there");
+        });
+    }
+}
