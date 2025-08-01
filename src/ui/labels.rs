d294f295ecdf67f6d4b0154849a148a8a75ae849
Bob Mannino <manninobobby@icloud.com>
2025-08-01 20:07:38 +0100

    ui: update h1/h2 styles

--- src/ui/labels.rs
+++ src/ui/labels.rs
@@ -1,4 +1,4 @@
-use egui::{RichText, Widget};
+use egui::{Color32, RichText, Widget};
 
 pub struct H1 {
     text: String,
@@ -12,6 +12,32 @@ impl H1 {
 
 impl Widget for H1 {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        ui.label(RichText::new(self.text).strong())
+        let rt = RichText::new(self.text)
+            .color(Color32::BLACK)
+            .size(40.)
+            .strong();
+
+        ui.label(rt)
+    }
+}
+
+pub struct H2 {
+    text: String,
+}
+
+impl H2 {
+    pub fn new(text: String) -> Self {
+        Self { text }
+    }
+}
+
+impl Widget for H2 {
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        let rt = RichText::new(self.text)
+            .color(Color32::BLACK)
+            .size(28.)
+            .strong();
+
+        ui.label(rt)
     }
 }
